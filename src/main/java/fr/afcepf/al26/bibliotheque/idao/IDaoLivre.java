677dab0c7b053f13bbb1c3f69c5aade8e1e9c794
Alexandre QUERE <alexquere1@gmail.com>
2016-01-04 11:39:19 +0100

    ajout de la methode listLivre

--- src/main/java/fr/afcepf/al26/bibliotheque/idao/IDaoLivre.java
+++ src/main/java/fr/afcepf/al26/bibliotheque/idao/IDaoLivre.java
@@ -2,8 +2,10 @@ package fr.afcepf.al26.bibliotheque.idao;
 
 import fr.afcepf.al26.bibliotheque.entity.Livre;
 
+import java.util.List;
+
 public interface IDaoLivre {
 
 	 void ajouterLivre(Livre livre);
-
+	List<Livre> getLivreParAuteur(int idAuteur);
 }
--- src/main/java/fr/afcepf/al26/bibliotheque/jdbc/dao/DaoLivre.java
+++ src/main/java/fr/afcepf/al26/bibliotheque/jdbc/dao/DaoLivre.java
@@ -6,9 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
-import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
 
 import fr.afcepf.al26.bibliotheque.entity.Livre;
 import fr.afcepf.al26.bibliotheque.idao.IDaoLivre;
@@ -16,19 +17,11 @@ import fr.afcepf.al26.bibliotheque.jdbc.Al26DataSource;
 
 public class DaoLivre implements IDaoLivre {
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see
-	 * fr.afcepf.al26.bibliotheque.jdbc.dao.IDaoLivre#ajouterLivre(fr.afcepf
-	 * .al26.bibliotheque.entity.Livre)
-	 */
+	private Connection cnx;
+	private DataSource ds;
 	@Override
 	public void ajouterLivre(Livre livre) {
-
-		Connection cnx = null;
-		DataSource ds = new Al26DataSource();
-
+		ds = new Al26DataSource();
 		try {
 			cnx = ds.getConnection();
 			String requete = "INSERT INTO LIVRE "
@@ -56,10 +49,46 @@ public class DaoLivre implements IDaoLivre {
 			try {
 				cnx.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 
 	}
+
+	@Override
+	public List<Livre> getLivreParAuteur(int idAuteur) {
+        List<Livre> livreList = new ArrayList<Livre>();
+		String requete = "SELECT * FROM LIVRE WHERE id_auteur = ?";
+		ds = new Al26DataSource();
+		try {
+			cnx = ds.getConnection();
+			PreparedStatement preparedStatement = cnx.prepareStatement(requete);
+			preparedStatement.setInt(1,idAuteur);
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()){
+                livreList.add(hydrateLivre(rs));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        finally {
+            try {
+                cnx.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return livreList;
+	}
+
+    private Livre hydrateLivre(ResultSet rs) throws SQLException {
+        Livre livre = new Livre();
+        java.util.Date date = new java.util.Date(rs.getDate("date_parution").getTime());
+        livre.setDateParution(date);
+        livre.setIdLivre(rs.getInt("id_livre"));
+        livre.setNbPages(rs.getInt("nombre_pages"));
+        livre.setEdition(rs.getString("edition"));
+        livre.setTitre(rs.getString("titre"));
+        return livre;
+    }
 }
