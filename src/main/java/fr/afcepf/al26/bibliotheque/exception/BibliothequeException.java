a39fd109b05993df05e83754bb8ffbbbfabc2359
Alexandre QUERE <alexquere1@gmail.com>
2016-01-05 11:15:27 +0100

    dev sur la classe exception

--- src/main/java/fr/afcepf/al26/bibliotheque/exception/BibliothequeException.java
+++ src/main/java/fr/afcepf/al26/bibliotheque/exception/BibliothequeException.java
@@ -4,4 +4,17 @@ package fr.afcepf.al26.bibliotheque.exception;
  * Created by Stagiaire on 05/01/2016.
  */
 public class BibliothequeException extends Exception {
+
+    public BibliothequeException() {
+        super("problème lors de la vérification de l'unicité du mail ou du pseudo");
+    }
+
+    public BibliothequeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
 }
